e00906d04d5b687f069c3a8b2359c2a80b79850d
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-02-26 20:42:08 +0700

    add bookinfo command

---
+++ commands/utility/bookinfo.js
@@ -0,0 +1,78 @@
+const goodreads = require("goodreads-api-node");
+const myCredentials = {
+  key: process.env.GOODREADS_KEY,
+  secret: process.env.GOODREADS_SECRET,
+};
+const gr = goodreads(myCredentials);
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "bookinfo",
+  description: "get a book's information from goodreads api",
+  aliases: ["book"],
+  execute(message, args) {
+    const sendMessage = (bookData) => {
+      const getAuthor = () => {
+        authors = bookData.authors.author;
+        if (Array.isArray(authors)) {
+          return authors[0].name;
+        } else {
+          return authors.name;
+        }
+      };
+
+      const getImageUrl = () => {
+        imageUrl = bookData.image_url;
+        if (imageUrl) return imageUrl;
+        return "https://via.placeholder.com/300";
+      };
+
+      const getDescription = () => {
+        bookDescription = bookData.description
+          .split("<br />")
+          .join("\n")
+          .split("<i>")
+          .join("*")
+          .split("</i>")
+          .join("*");
+        if (bookDescription.length > 1000) {
+          bookDescription = bookDescription.slice(0, 1000);
+          bookDescription += "...";
+        }
+        if (bookDescription) return bookDescription;
+        return "no description";
+      };
+
+      const embedMessage = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(bookData.title)
+        .setURL(bookData.url)
+        .setThumbnail(getImageUrl())
+        .addField("Author", getAuthor())
+        .addField("Description", getDescription())
+        .setFooter("RoyahBot (Beta) • Data Provided by Goodreads")
+        .setTimestamp();
+      return message.channel.send(embedMessage);
+    };
+
+    const showBook = (bookId) => {
+      message.channel.send(`Found! Getting book data...`);
+      gr.showBook(bookId).then((data) => {
+        bookData = data.book;
+        sendMessage(bookData);
+      });
+    };
+
+    const query = args.join(" ");
+    message.channel.send(`Searching for *${query}*...`);
+    gr.searchBooks({ q: query, field: "all" }).then((data) => {
+      results = data.search.results.work;
+      if (Array.isArray(results)) {
+        bookId = results[0].best_book.id._;
+      } else {
+        bookId = results.best_book.id._;
+      }
+      showBook(bookId);
+    });
+  },
+};
--- index.js
+++ index.js
@@ -32,9 +32,13 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(commandName)) return;
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
 
-  const command = client.commands.get(commandName);
+  if (!command) return;
 
   //check if a command cannot be executed inside dm
   if (command.guildOnly && message.channel.type === "dm") {
