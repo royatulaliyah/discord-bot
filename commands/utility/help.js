329db15246941c11a2d45801195c2fecd96b645b
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-03-24 08:35:18 +0700

    update help link

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,9 +1,18 @@
+const Discord = require("discord.js");
 module.exports = {
   name: "help",
   description: "Bantuan",
   execute(message, args) {
-    message.channel.send(
-      "Lihat bantuan di sini:\nhttps://royatulaliyah.netlify.app/discord"
-    );
+    const embed = new Discord.MessageEmbed()
+      .attachFiles(["src/images/logo.png"])
+      .setColor("RANDOM")
+      .setTitle("Bantuan")
+      .setURL(
+        "https://itdev.royah.me/Discord-Bot-ee2e07e68e4646898bf7e4bccd2f25b8"
+      )
+      .setTimestamp()
+      .setFooter("RoyahBot", "attachment://logo.png");
+
+    return message.channel.send(embed);
   },
 };
