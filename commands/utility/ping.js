1b8ae52387a262e61272e40e01973380d2d4a30b
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-05-24 13:04:29 +0700

    Update ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,6 +2,9 @@ module.exports = {
   name: "ping",
   description: "Ping!",
   execute(message, args) {
-    message.channel.send("pong");
+    message.channel.send("<a:ping:846264946907545602>");
+    message.channel.send("Pinging...").then((sent) => {
+      sent.edit(`${sent.createdTimestamp - message.createdTimestamp}ms`);
+    });
   },
 };
