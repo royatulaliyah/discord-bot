90259d2d4a03abc3a43decccece47e97a98428af
Muhammad Urwatil Wutsqo <60605671+wutsqo@users.noreply.github.com>
2021-05-26 01:07:59 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,17 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL + "?ssl=true");
+//const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false
+      }
+    }
+  }
+);
 const Fess = sequelize.define("Fess", {
   authorId: {
     type: DataTypes.STRING,
