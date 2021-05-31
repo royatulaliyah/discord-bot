855cc3074dc64f4295cddcc14a247d1cd05d1e87
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-05-31 14:06:21 +0700

    disable sequelize logging

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const { Sequelize, DataTypes } = require("sequelize");
 //const sequelize = new Sequelize(process.env.DATABASE_URL);
 const sequelize = new Sequelize(
-  process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
-         require: true,
-         rejectUnauthorized: false
-      }
-    }
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
   }
 );
 const Fess = sequelize.define("Fess", {
